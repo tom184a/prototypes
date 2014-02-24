87f7f7571436468e78b76cbd97d076766ddcc529
Tomasz Sikora <tom184a@gmail.com>
2014-02-24 19:44:49 +0100

    Initial commit

---
+++ Networking/Executor.cc
@@ -0,0 +1,19 @@
+/*
+ * Executor.cc
+ *
+ *  Created on: 24 lut 2014
+ *      Author: tomek
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Execute something" << endl;
+
+	return 0;
+}
+
+
+
+
